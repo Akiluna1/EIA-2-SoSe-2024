bed34d8d64e37de126e1fb83ef5ed3e2808364b6
Akiluna1 <149687798+Akiluna1@users.noreply.github.com>
2024-03-30 17:23:30 +0100

    Add files via upload

---
+++ A01_RandomPoem/RandomPoem.js
@@ -0,0 +1,38 @@
+"use strict";
+/*
+Aufgabe: L01_Zufallsgedicht
+Name: Elisabeth Krumm
+Matrikel: <Deine Matrikelnummer> 275810
+Datum: 30.03.2024
+Quellen:
+*/
+var RandomPoem;
+(function (RandomPoem) {
+    let subjekte = ['Harry', 'Hermine', 'Hagrid', 'Snape', 'Eli', 'Cathi'];
+    let prädikate = ['braut', 'liebt', 'studiert', 'hasst', 'zaubert', 'zerstört'];
+    let objekte = ['Zaubertränke', 'den Grimm', 'Lupin', 'Hogwarts', 'die Karte des Rumtreibers', 'Dementoren'];
+    // console.log(subjekte);
+    // console.log(prädikate);
+    // console.log(objekte);
+    for (let i = subjekte.length; i >= 1; i--) {
+        // console.log(i);
+        console.log(getVerse(subjekte, prädikate, objekte));
+    }
+    function getVerse(_subjekte, _prädikate, _objekte) {
+        let vers = "";
+        let randomSubjectIndex = Math.floor(Math.random() * _subjekte.length);
+        // console.log(randomSubjectIndex);
+        let randomSubject = _subjekte.splice(randomSubjectIndex, 1)[0];
+        vers += randomSubject + " ";
+        let randomPredicateIndex = Math.floor(Math.random() * _prädikate.length);
+        // console.log(randomPredicateIndex);
+        let randomPredicate = _prädikate.splice(randomPredicateIndex, 1)[0];
+        vers += randomPredicate + " ";
+        let randomObjectIndex = Math.floor(Math.random() * _objekte.length);
+        // console.log(randomObjectIndex);
+        let randomObject = _objekte.splice(randomObjectIndex, 1)[0];
+        vers += randomObject;
+        return vers;
+    }
+})(RandomPoem || (RandomPoem = {}));
+//# sourceMappingURL=RandomPoem.js.map
